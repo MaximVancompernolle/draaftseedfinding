dabaed096763c5d57e2d68633333f30520302b32
PacManMVC <maxim.vancompernolle@gmail.com>
2025-04-26 17:09:04 -0400

    finish file, incremental, and random searching structure

--- src/main/java/com/mvc/Config.java
+++ src/main/java/com/mvc/Config.java
@@ -4,17 +4,24 @@ import com.seedfinding.mccore.version.MCVersion;
 
 import java.io.File;
 
+import static com.mvc.Config.FILTER_TYPE.*;
+
 public class Config {
     public static final int SEED_MATCHES = 10_000;
     public static final int LOG_DELAY = 10_000;
     public static final MCVersion VERSION = MCVersion.v1_16_1;
-    public static final boolean FILTER_FILE = true;
+    public static final FILTER_TYPE FILTER = RANDOM;
     public static final File INPUT_FILE = new File("./src/main/resources/ssvFastionEndCityShipSeeds.txt");
     public static final File OUTPUT_FILE = new File("./src/main/resources/output.txt");
-    public static final boolean FILTER_INCREMENTAL = false;
     public static final int VILLAGE_DISTANCE = 8;
     public static final int TEMPLE_DISTANCE = 8;
     public static final int BASTION_DISTANCE = 12;
     public static final int FORTRESS_DISTANCE = 12;
     public static final int END_CITY_DISTANCE = 8;
+
+    public enum FILTER_TYPE {
+        FILE,
+        INCREMENTAL,
+        RANDOM
+    }
 }
--- src/main/java/com/mvc/Main.java
+++ src/main/java/com/mvc/Main.java
@@ -3,62 +3,94 @@ package com.mvc;
 import com.mvc.filters.structure.OverworldStructureFilter;
 import com.seedfinding.mccore.rand.ChunkRand;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    private static long seedsChecked = 0;
+    private static int seedMatches = 0;
+    private static long nextTime = 0;
+    private static long currentTime;
+    private static FileWriter output;
     public static void main(String[] args) throws IOException {
-        System.out.println("Starting seed finding...");
+        initialize();
 
-        if (Config.FILTER_FILE) {
-            filterFile(Config.INPUT_FILE, Config.OUTPUT_FILE);
-        } else if (Config.FILTER_INCREMENTAL) {
+        if (Config.FILTER.equals(Config.FILTER_TYPE.FILE)) {
+            filterFile();
+        } else if (Config.FILTER.equals(Config.FILTER_TYPE.INCREMENTAL)) {
             filterIncremental();
+        } else if (Config.FILTER.equals(Config.FILTER_TYPE.RANDOM)) {
+            filterRandom();
         } else {
             throw new RuntimeException("Define filter type as FILE or INCREMENTAL in com.mvc.Config");
         }
 
+        finish();
     }
 
-    public static void filterFile(File inputFile, File outputFile) throws IOException {
-        Scanner scanner = new Scanner(inputFile);
-        FileWriter output = new FileWriter(outputFile);
-
-        long seedsChecked = 0;
-        int seedMatches = 0;
-        long nextTime = 0;
-        long currentTime;
+    private static void filterFile() throws IOException {
+        Scanner scanner = new Scanner(Config.INPUT_FILE);
 
         while (scanner.hasNextLong() && seedMatches < Config.SEED_MATCHES) {
-            long structureSeed = scanner.nextLong();
-            long matchedStructureSeed = filterStructureSeed(structureSeed) ? structureSeed : 0L;
-
-            if (matchedStructureSeed != 0L) {
-                output.write(matchedStructureSeed + "\n");
-                seedMatches++;
-            }
-            seedsChecked++;
-            currentTime = System.currentTimeMillis();
-
-            if (currentTime > nextTime) {
-                nextTime = currentTime + Config.LOG_DELAY;
-                System.out.printf("%,d seeds checked with %,d matches\r", seedsChecked, seedMatches);
-            }
+            checkSeed(scanner.nextLong());
         }
-        output.close();
-        System.out.printf("%,d seeds checked with %,d matches\r", seedsChecked, seedMatches);
     }
 
-    public static void filterIncremental() {
-
+    private static void filterIncremental(long start) throws IOException {
+        while (seedMatches < Config.SEED_MATCHES) {
+            checkSeed(start);
+            start++;
+        }
     }
 
-    public static boolean filterStructureSeed(long structureSeed) {
+    private static void filterIncremental() throws IOException {
+        filterIncremental(0L);
+    }
+
+    private static void filterRandom() throws IOException {
+        Random random = new Random();
+
+        while (seedMatches < Config.SEED_MATCHES) {
+            checkSeed(random.nextLong());
+        }
+    }
+
+    private static void checkSeed(long seed) throws IOException {
+        Long matchedStructureSeed = filterStructureSeed(seed) ? seed : null;
+
+        if (matchedStructureSeed != null) {
+            output.write(matchedStructureSeed + "\n");
+            seedMatches++;
+        }
+        seedsChecked++;
+        currentTime = System.currentTimeMillis();
+
+        if (currentTime > nextTime) {
+            nextTime = currentTime + Config.LOG_DELAY;
+            System.out.printf("%,d seeds checked with %,d matches\r", seedsChecked, seedMatches);
+        }
+    }
+
+    private static boolean filterStructureSeed(long structureSeed) {
         ChunkRand chunkRand = new ChunkRand(structureSeed);
         OverworldStructureFilter overworldStructureFilter = new OverworldStructureFilter(structureSeed, chunkRand);
 
         return overworldStructureFilter.filterStructures();
+    }
+
+    private static void initialize() throws IOException {
+        System.out.println("Starting seed finding...");
+        seedsChecked = 0;
+        seedMatches = 0;
+        nextTime = 0;
+        currentTime = System.currentTimeMillis();
+        output = new FileWriter(Config.OUTPUT_FILE);
+    }
+
+    private static void finish() throws IOException {
+        output.close();
+        System.out.printf("%,d seeds checked with %,d matches\r", seedsChecked, seedMatches);
     }
 }
