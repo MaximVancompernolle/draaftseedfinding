64a17fa172d714b762befe05e953d9f2eff7a1d9
PacManMVC <maxim.vancompernolle@gmail.com>
2025-04-26 16:07:57 -0400

    add end ship filter

--- src/main/java/com/mvc/filters/structure/EndStructureFilter.java
+++ src/main/java/com/mvc/filters/structure/EndStructureFilter.java
@@ -1,4 +1,66 @@
 package com.mvc.filters.structure;
 
+import com.mvc.Config;
+import com.seedfinding.mcbiome.source.EndBiomeSource;
+import com.seedfinding.mccore.rand.ChunkRand;
+import com.seedfinding.mccore.util.pos.BPos;
+import com.seedfinding.mccore.util.pos.CPos;
+import com.seedfinding.mccore.util.pos.RPos;
+import com.seedfinding.mcfeature.structure.EndCity;
+import com.seedfinding.mcfeature.structure.generator.structure.EndCityGenerator;
+import com.seedfinding.mcterrain.terrain.EndTerrainGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class EndStructureFilter {
+    private final long structureSeed;
+    private final ChunkRand chunkRand;
+    private BPos gatewayPos;
+    private CPos cityPos;
+
+    public EndStructureFilter(long structureSeed, ChunkRand chunkRand) {
+        this.structureSeed = structureSeed;
+        this.chunkRand = chunkRand;
+    }
+
+    public boolean filterStructures() {
+        firstGatewayPos();
+
+        return hasCity() && hasShip();
+    }
+
+    private void firstGatewayPos() {
+        ArrayList<Integer> gateways = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            gateways.add(i);
+        }
+        Collections.shuffle(gateways, new Random(structureSeed));
+        double angle = 2.0 * (-1 * Math.PI + 0.15707963267948966 * (gateways.removeLast()));
+        int x = (int) (1024.0 * Math.cos(angle));
+        int z = (int) (1024.0 * Math.sin(angle));
+
+        gatewayPos = new BPos(x, 0, z);
+    }
+
+    private boolean hasCity() {
+        RPos gatewayRegion = gatewayPos.toRegionPos(20 << 4);
+        EndCity city = new EndCity(Config.VERSION);
+        cityPos = city.getInRegion(structureSeed, gatewayRegion.getX(), gatewayRegion.getZ(), chunkRand);
+
+        return cityPos.getMagnitude() <= Config.END_CITY_DISTANCE;
+    }
+
+    private boolean hasShip() {
+        EndCityGenerator ecg = new EndCityGenerator(Config.VERSION);
+        EndBiomeSource endBiomeSource = new EndBiomeSource(Config.VERSION, structureSeed);
+        EndTerrainGenerator endTerrainGenerator = new EndTerrainGenerator(endBiomeSource);
+
+        if (!ecg.generate(endTerrainGenerator, cityPos, chunkRand)) {
+            return false;
+        }
+
+        return ecg.hasShip();
+    }
 }
